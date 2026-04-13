6485d98a4b433fd21272e70934fbd2f5b22a2dd3
aroido-bigcat <bigcat@aroido.com>
2026-04-13 23:42:41 +0900

    tokenmon v0.1.2

--- Casks/tokenmon.rb
+++ Casks/tokenmon.rb
@@ -1,11 +1,13 @@
 cask "tokenmon" do
-  version "0.1.1"
-  sha256 "39d21c4bec0dd1a76911a59a61884bf146813e373889c81028714060327b228b"
+  version "0.1.2"
+  sha256 "268005af41db945d3cad7bd0d6d268195c34b614659e340a0faccfb5ca9a48f8"
 
   url "https://github.com/aroido/tokenmon/releases/download/v#{version}/Tokenmon-#{version}.dmg"
   name "Tokenmon"
   desc "Offline-first menu bar creature companion for local AI coding"
   homepage "https://github.com/aroido/tokenmon"
+
+  auto_updates true
 
   depends_on macos: ">= :sonoma"
 
